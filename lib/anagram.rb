9e266868735bf8db782b8309c1ce45a3d1b7d959
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-23 16:06:33 -0500

    commit tree f44fb507b39fe1368a7a332fd67841d81f8ef2d2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match (array)
+      my_letters=@word.chars
+      array.filter do |w|
+        if w.chars.sort==my_letters.sort
+            w
+        elsif w.chars.sort!=my_letters.sort
+           nil
+      end
+    end
+  end
+end
+
+binding.pry
+0
